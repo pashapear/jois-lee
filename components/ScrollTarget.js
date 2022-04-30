871cb89ce682124ee0bfac5d5f620cd753ead45e
Pasha Petrosyan <pashacodes@gmail.com>
2022-04-30 12:41:46 -0500

    fix: forward ref for scroll target

--- components/ScrollTarget.js
+++ components/ScrollTarget.js
@@ -1,5 +1,11 @@
+import React from "react";
+
 // parent container must be relative positioned
 
-export const ScrollTarget = ({ id, offset }) => (
-	<div id={id} style={{ position: "absolute", top: offset, left: 0 }}>{``}</div>
-);
+export const ScrollTarget = React.forwardRef(({ id, offset }, ref) => (
+	<div
+		id={id}
+		style={{ position: "absolute", top: offset, left: 0 }}
+		ref={ref}
+	>{``}</div>
+));
